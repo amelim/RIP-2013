7c4caffa0e8ff38c9dbe4bbc4d97907d2d495ae8
Daniel Pickem <daniel.pickem@gatech.edu>
2013-09-20 16:20:27 -0400

    updated helloWorld for testing A*

--- cpp/scripts/helloWorld.cpp
+++ cpp/scripts/helloWorld.cpp
@@ -1,5 +1,7 @@
 #include "location.h"
 #include "world.h"
+#include "state.h"
+#include "a_star.h"
 
 #include <iostream>
 
@@ -8,7 +10,7 @@ using namespace std;
 int main(){
 	/* Test location class */
 	Location loc(1, 3);
-	loc.print();
+	//loc.print();
 
 	/* Test world class */
 	int i,j;
@@ -18,7 +20,7 @@ int main(){
 		tmp.clear();
 
 		for (j = 0; j < 10; j++) {
-			tmp.push_back(BOX);
+			tmp.push_back(EMPTY);
 		}
 
 		map.push_back(tmp);
@@ -27,12 +29,23 @@ int main(){
 	vector<Location> boxes;
 	for (i = 0; i < 5; i++) {
 		boxes.push_back(Location(i,i));
+		map.at(i).at(i) = BOX;
+		map.at(i+1).at(i) = TARGET;
 	}
 
-	World world(&map, new Location(1,1), &boxes, &boxes);
-	world.printWorld();
-	world.printConfig();
+	map.at(1).at(5) = ROBOT;
 
-  cout << "Hello World!" << endl;
+	World world(&map, new Location(1,1), &boxes, &boxes);
+	//world.printWorld();
+	//world.printConfig();
+
+	State state(world);
+	//cout << state;
+
+	AStar astar(state);
+	cout << "Output of AStar::printClosed()" << endl;
+	astar.printClosed();
+
+  	cout << "Hello World!" << endl;
   return 1;
 }
