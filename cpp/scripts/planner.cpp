16a6f325d723e364531ccf2739e5fd84a8ecbb0d
jtillma3 <jtillma3@gmail.com>
2013-09-24 14:23:49 -0400

    Prelim Update

--- cpp/scripts/planner.cpp
+++ cpp/scripts/planner.cpp
@@ -1,14 +1,16 @@
 //TODO: 
 //finish full function implementation
 //allow output to solution file
+//Modify README to match changes to naming
+//Remove output file reference
 #include "location.h"
 #include "world.h"
 #include "state.h"
 #include "a_star.h"
 #include <iostream>
 #include <cstdio>
+#include "parser.h"
 using namespace std;
-//Mattheus's Files
 
 
 int main(int argc, char** argv){
@@ -22,28 +24,45 @@ if(iwFile ==NULL) {
 cout <<"Error opening inputWorld, make sure the file exists.\n\n";
 return 2;
 }
+fclose(iwFile);
+Parser parser(iwFile);
+Matrix map=parser.loadMap();
+int i,j;
+vector<Location>BoxesInit;
+vector<Location>BoxesTarget;
+//Find Location Robot in Matrix
+bool RobotExists=False;
+for (i=0;i<parser.getRows();i++){
+	for(j=0;j<parser.getColumns();j++){
+		if(map[i][j] == BOX{
+		BoxesInit.push_back(Location(i,j));
+		}else if(map[i][j] == TARGET){
+			BoxesTarget.push_back(Location(i,j));
+		}else if (map[i][j] == ROBOT){
+			Location RobotInit(i,j)
+			RobotExists=True;
+		}
+	}
+}
+if(RobotExists == False){
+cout<<"No robot located on the map";
+return 3;
 
-//Create Matrices world from inputWorld
-//Location robotInit()
-//Matrix inputMap = CreateWorld(iwFile);
-//vector<Location> boxesInit= inputWorld.getBoxesInit();
-//vector <Location> boxesTarget = inputWorld.getBoxesTarget();
-//World inputWorld(inputMap,robotInit,boxesInit,boxesTarget);
-//State state(inputWorld);
+}
+World inputWorld(map,RobotInit,BoxesInit,BoxesTarget);
+State state(inputWorld);
 //TODO: add error catching for world generation
-//cout <<"Generated input world sucessfully.\n\n";
+cout <<"Generated input world sucessfully.\n\n";
 
-//cout <<state;
+cout <<state;
 
-//AStar astar(state);
-//astar.solve();
-//astar.printSolution();
+AStar astar(state);
+astar.solve();
+astar.printSolution();
 
 
 
 fclose(iwFile);
-//#ifdef outputFile
-//fclose(outputFile);
-//#endif
+
 return 0;
 }
