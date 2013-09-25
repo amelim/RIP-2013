72685b2365c9e5be4a5119d7a1df1c4e96ecd825
jtillma3 <jtillma3@gmail.com>
2013-09-24 21:25:16 -0400

    Added print to planner.cpp to check map flip

--- cpp/scripts/planner.cpp
+++ cpp/scripts/planner.cpp
@@ -11,7 +11,17 @@
 #include <cstdio>
 #include "parser.h"
 using namespace std;
+Matrix* FlipMatrix(Matrix *m, int rows,int cols){
+Matrix* x;
+x->resize(rows*cols);
+	for(int i =0;i<rows;i++){
+		for(int j=0;j<cols;j++){
+			x[i][j] = m[j][i];
+}
+}
 
+return x;
+}
 
 int main(int argc, char** argv){
 if(argc <2){
@@ -25,31 +35,45 @@ cout <<"Error opening inputWorld, make sure the file exists.\n\n";
 return 2;
 }
 fclose(iwFile);
-Parser parser(iwFile);
+Parser parser(iwString);
 Matrix map=parser.loadMap();
+int rows = parser.getRows();
+int cols = parser.getColumns();
 int i,j;
+for (i=0;i<rows;i++){
+for(j=0;j<cols;j++){
+printf("%d",map[i][j]);
+printf(" ");
+if(j==cols-1)printf("\n");
+
+
+}
+}
+
+Location RobotInit;
+//Matrix *test=FlipMatrix(&map,rows,cols);
 vector<Location>BoxesInit;
 vector<Location>BoxesTarget;
 //Find Location Robot in Matrix
-bool RobotExists=False;
+bool RobotExists=false;
 for (i=0;i<parser.getRows();i++){
 	for(j=0;j<parser.getColumns();j++){
-		if(map[i][j] == BOX{
+		if(map[i][j] == BOX){
 		BoxesInit.push_back(Location(i,j));
 		}else if(map[i][j] == TARGET){
 			BoxesTarget.push_back(Location(i,j));
 		}else if (map[i][j] == ROBOT){
-			Location RobotInit(i,j)
-			RobotExists=True;
+			RobotInit = Location(i,j);
+			RobotExists=true;
 		}
 	}
 }
-if(RobotExists == False){
+if(RobotExists == false){
 cout<<"No robot located on the map";
 return 3;
 
 }
-World inputWorld(map,RobotInit,BoxesInit,BoxesTarget);
+World inputWorld(&map,&RobotInit,&BoxesInit,&BoxesTarget);
 State state(inputWorld);
 //TODO: add error catching for world generation
 cout <<"Generated input world sucessfully.\n\n";
